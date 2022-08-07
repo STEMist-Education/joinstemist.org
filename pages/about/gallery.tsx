2af12e48ac3a80a2209acb8983dcb9453cccaf3c
snek152 <kakanisnehil@gmail.com>
2022-08-07 10:07:46 -0700

    fixing gallery image size

--- pages/about/gallery.tsx
+++ pages/about/gallery.tsx
@@ -21,131 +21,94 @@ interface Photo {
 const photos: Photo[] = [
   {
     src: "/about/carousel2.png",
-    width: 4,
-    height: 2,
     alt: "Selena teaches students about DNA Analysis in the Intro to STEM workshop of April 2022.",
   },
   {
     src: "/about/gettingstartedrepljs.png",
-    width: 3,
-    height: 2,
     alt: "Snehil helps students get set up with their coding environment in the 2022 Summer Course.",
   },
   {
     src: "/about/carousel5.png",
-    width: 2,
-    height: 1,
     alt: "Steve talks about the basics of waves in the Intro to STEM workshop of April 2022.",
   },
   {
     src: "/about/bioday4.png",
-    width: 3,
-    height: 2,
     alt: "Chaas introduces the human body's circulatory system in the 2022 Summer Course.",
   },
   {
     src: "/about/strings.png",
-    width: 3,
-    height: 4,
     alt: "Shanker teaches students about the concept of strings in Python in the 2022 Summer Course.",
   },
   {
     src: "/about/carousel1.png",
-    width: 4,
-    height: 3,
     alt: "Snehil discusses the difference between backend and frontend web development in the Intro to STEM workshop of April 2022.",
   },
   {
     src: "/about/cppmore.png",
-    width: 4,
-    height: 3,
     alt: "Daniel works through a Codeforces programming problem using C++ in the 2022 Summer Course.",
   },
   {
     src: "/about/yes yes.png",
-    width: 3,
-    height: 2,
     alt: "Shanker works through a coding problem live in the 2022 Summer Course.",
   },
   {
     src: "/about/carousel4.png",
-    width: 4,
-    height: 2,
     alt: "Selena introduces the concept of DNA cloning using bacteria in the Intro to STEM workshop of April 2022.",
   },
   {
     src: "/about/1.png",
-    width: 4,
-    height: 3,
     alt: "Shanker introduces binary numbers and how they go hand in hand with programming in the 2022 Summer Course.",
   },
   {
     src: "/about/day 4 js.png",
-    width: 3,
-    height: 2,
     alt: "Snehil helps students out as they build their final Javascript project in the 2022 Summer Course.",
   },
   {
     src: "/about/cat class.png",
-    width: 4,
-    height: 3,
     alt: "Shanker explains a guided problem in the 2022 Summer Course.",
   },
   {
     src: "/about/cppday1.png",
-    width: 1,
-    height: 2,
     alt: "Justin gives students a review problem to make a C++ calculator in the 2022 Summer Course.",
   },
   {
     src: "/about/ecologyend.png",
-    width: 3,
-    height: 4,
     alt: "Selena congratulates the winners of the Ecology kahoot in the 2022 Summer Course.",
   },
   {
     src: "/about/carousel3.png",
-    width: 3,
-    height: 2,
     alt: "Snehil goes over the varies methods of how to get started with backend programming in the Intro to STEM workshop of April 2022.",
   },
   {
     src: "/about/python day 5.2.png",
-    width: 3,
-    height: 2,
     alt: "Shanker talks about advanced machine learning concepts in the 2022 Summer Course.",
   },
   {
     src: "/about/day6botany.png",
-    width: 4,
-    height: 3,
     alt: "Harold introduces the concept of botany in the 2022 Summer Course.",
   },
   {
     src: "/about/python day 5.png",
-    width: 4,
-    height: 3,
     alt: "Shanker explains the concept of neural networks in the 2022 Summer Course.",
   },
   {
     src: "/about/day2biop2.png",
-    width: 3,
-    height: 2,
     alt: "Selena prepares students for the challenging topic of metabolism in the 2022 Summer Course.",
   },
   {
     src: "/about/python day 6.png",
-    width: 4,
-    height: 3,
     alt: "Aadharsh assists students in their final Python project in the 2022 Summer Course.",
   },
   {
     src: "/about/carousel6.png",
-    width: 1,
-    height: 1,
     alt: "Steve congratulates the top 5 participants in his Waves kahoot in the Intro to STEM workshop of April 2022.",
   },
-];
+].map((photo) => ({
+  ...photo,
+  width: 2,
+  height: 1,
+}));
+
 export default function About() {
   const [open, setOpen] = useState<boolean>(false);
   const [currentPhoto, setCurrentPhoto] = useState<Photo>(photos[0]);
